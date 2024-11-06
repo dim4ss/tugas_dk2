8cdc8ecb240aaf014b47286d0a90df4509357008
dim4ss <dimasghifaria@gmail.com>
2024-11-06 10:41:26 +0700

    memperbaiki format pada output hasil perhitungan.

--- main.py
+++ main.py
@@ -7,5 +7,11 @@ lebar = float(input("masukan lebar persegi panjang:"))
 #menghitung luas
 luas = panjang * lebar
 
+#menghitung keliling
+
 #menampilkan hasil luas
-print(f"luas persegi panjang:{luas}")
+keliling = 2 * (panjang + lebar )
+
+#menampilkan hasil luas dan keliling
+print(f"luas persegi panjang: {luas}")
+print(f"keliling persegi panjang adalah: {keliling}")
