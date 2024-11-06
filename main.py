e82cd1ade80220261b35b873483f862ceb022e90
dim4ss <dimasghifaria@gmail.com>
2024-11-06 11:15:50 +0700

    Merge branch validasi-input ke master.

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 # main.py
 
+#fungsi untuk memeriksa apakah input adalah bilangan pasitif 
+def input_positif(prompt):
+    while True:
+        try:
+            nilai = float(input(prompt))
+            if nilai > 0:
+                return nilai
+            else:
+                print("Nilai harus lebih dari 0. coba lagi.")
+        except ValueError:
+            print("Input tidak valid.masukan angka.")
+
+
 #meminta input panjang dan lebar dari pengguna
-panjang = float(input("masukan panjang persegi panjang:"))
-lebar = float(input("masukan lebar persegi panjang:"))
+Panjang = float(input("masukan panjang persegi panjang:"))
+Lebar = float(input("masukan lebar persegi panjang:"))
 
 #menghitung luas
-luas = panjang * lebar
+Luas = Panjang * Lebar
 
 #menghitung keliling
 
 #menampilkan hasil luas
-keliling = 2 * (panjang + lebar )
+keliling = 2 * (Panjang + Lebar )
 
 #menampilkan hasil luas dan keliling
-print(f"luas persegi panjang: {luas} satuan persegi")
-print(f"keliling persegi panjang adalah: {keliling} satuan")
+print(f"Luas persegi panjang: {Luas} satuan persegi")
+print(f"Keliling persegi panjang adalah: {Keliling} satuan")
